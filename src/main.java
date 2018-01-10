d52f3be10bb15a6e1257170da6e54161d3144803
chongjae.yoo <chongjae.yoo@lge.com>
2018-01-10 17:20:17 +0900

    Fix buy logic

--- src/main.java
+++ src/main.java
@@ -1,9 +1,12 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.FileHandler;
@@ -17,6 +20,7 @@ import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.webcerebrium.binance.api.BinanceApi;
 import com.webcerebrium.binance.api.BinanceApiException;
+import com.webcerebrium.binance.api.BinanceRequest;
 import com.webcerebrium.binance.datatype.BinanceCandlestick;
 import com.webcerebrium.binance.datatype.BinanceInterval;
 import com.webcerebrium.binance.datatype.BinanceOrder;
@@ -27,10 +31,10 @@ import com.webcerebrium.binance.datatype.BinanceSymbol;
 
 public class main {
 
-	public static int minuteThreshold = 5 * 60;//
+	public static int minuteThreshold = 5 * 60;
 	public static double rapidThreshold = 1.02;
 	public static double sellThreshold = 1.03;
-	public static double constantSellThreshold = 1.07;
+	public static double constantSellThreshold = 1.1;
 	public static int sellCount = 2;
 	public static int buyCnt = 2;
 	public static int boughtCnt = 0;
@@ -58,12 +62,16 @@ public class main {
 		}
 		logger.info(String.valueOf(isReallyBuy));
 
+		initCoinInfo();
+
 		while (true) {
 			try {
 				if (needToupdateTotalBalance) {
 					totalBalance = new BinanceApi().balancesMap().get("ETH").free.doubleValue();
+					sendMsgToTelegram("Current Balance : " + totalBalance, true);
 					needToupdateTotalBalance = false;
 				}
+
 				JsonArray prices = new BinanceApi().allBookTickers();
 				for (int i = 0; i < prices.size(); i++) {
 					JsonObject price = prices.get(i).getAsJsonObject();
@@ -72,8 +80,6 @@ public class main {
 						continue;
 					}
 				}
-				HashMap<String, String> rapidSet = new HashMap<>();
-				HashMap<String, String> sellSet = new HashMap<>();
 				for (int i = 0; i < prices.size(); i++) {
 					JsonObject price = prices.get(i).getAsJsonObject();
 					String key = price.get("symbol").toString().replaceAll("\"", "");
@@ -100,8 +106,7 @@ public class main {
 
 						if (isRapidUp && coinInfo.buyPrice == 0 && boughtCnt < maxBuyCnt) {
 							coinInfo.buyPrice = curPrice;
-							boughtCnt++;
-							rapidSet.put(key, key + "이 급등하였습니다. Buy : " + curPrice);
+							sendMsgToTelegram(key + "이 급등하였습니다. Buy : " + curPrice, false);
 							if (isReallyBuy) {
 								buyCoin(coinInfo);
 							}
@@ -117,51 +122,40 @@ public class main {
 							double cutRate = coinInfo.buyPrice / curPrice;
 
 							if (cutRate >= constantSellThreshold) {
-								sellSet.put(key, key + "을 " + coinInfo.buyPrice + "에 매수하여, " + curPrice + "에 매도하였습니다. ("
-										+ cutRate + ") " + coinInfo.isReallyBuy);
-								coinInfo.buyPrice = 0;
+								cutRate = curPrice / coinInfo.buyPrice;
+								sendMsgToTelegram(key + "을 " + coinInfo.buyPrice + "에 매수하여, " + curPrice
+										+ "에 매도하였습니다. (" + cutRate + ") ", false);
 								curProfit += (100 * cutRate) - 100;
-								sendMsgToTelegram("Cur Profit : " + curProfit + ", Bought : \n" + getBoughtList());
-								boughtCnt--;
+								sendMsgToTelegram("Cur Profit : " + curProfit + ", Bought : \n" + getBoughtList(),
+										false);
 								coinInfo.cutPrice = curPrice;
-								if (coinInfo.isReallyBuy) {
+								if (isReallyBuy) {
 									sellCoin(coinInfo);
+								}
+								if (boughtCnt == 0) {
+									needToupdateTotalBalance = true;
 								}
 							} else if (rate > sellThreshold) {
 								double curRate = curPrice / coinInfo.buyPrice;
-								if (curRate > 1.01f) {
-									sellSet.put(key, key + "을 " + coinInfo.buyPrice + "에 매수하여, " + curPrice
-											+ "에 매도하였습니다. (" + curRate + ") " + coinInfo.isReallyBuy);
-									coinInfo.buyPrice = 0;
+								if (curRate > 1.03f) {
+									sendMsgToTelegram(key + "을 " + coinInfo.buyPrice + "에 매수하여, " + curPrice
+											+ "에 매도하였습니다. (" + curRate + ") ", false);
 									curProfit += (100 * curRate) - 100;
-									sendMsgToTelegram("Cur Profit : " + curProfit + ", Bought : \n" + getBoughtList());
-									boughtCnt--;
+									sendMsgToTelegram("Cur Profit : " + curProfit + ", Bought : \n" + getBoughtList(),
+											false);
 									coinInfo.cutPrice = curPrice;
-									if (coinInfo.isReallyBuy) {
+									if (isReallyBuy) {
 										sellCoin(coinInfo);
+									}
+									if (boughtCnt == 0) {
+										needToupdateTotalBalance = true;
 									}
 								}
 							}
 						}
-					} else {
-						CoinInfo coinInfo = new CoinInfo(key);
-						coins.put(key, coinInfo);
 					}
 				}
-				if (rapidSet.size() > 0) {
-					String ret = "";
-					for (String rapidKey : rapidSet.keySet()) {
-						ret += rapidSet.get(rapidKey) + "\n";
-					}
-					sendMsgToTelegram(ret);
-				}
-				if (sellSet.size() > 0) {
-					String ret = "";
-					for (String rapidKey : sellSet.keySet()) {
-						ret += rapidKey + " : " + sellSet.get(rapidKey) + "\n";
-					}
-					sendMsgToTelegram(ret);
-				}
+
 				Thread.sleep(1000);
 				logger.info("System is running....");
 			} catch (Exception e) {
@@ -183,8 +177,8 @@ public class main {
 		return ret.toString();
 	}
 
-	public static void sendMsgToTelegram(String msg) {
-		
+	public static void sendMsgToTelegram(String msg, boolean toMe) {
+
 		logger.info("Send msg to Telegram : " + msg);
 		Connection con = null;
 		Statement stat = null;
@@ -200,6 +194,13 @@ public class main {
 			boolean isDisableBuy = false;
 			while (ret.next()) {
 				String userId = ret.getString("user");
+
+				if (toMe) {
+					if (!"196764827".equals(userId)) {
+						continue;
+					}
+				}
+
 				if ("9999".equals(userId)) {
 					isDisableBuy = true;
 				}
@@ -217,15 +218,29 @@ public class main {
 			BinanceApi api = new BinanceApi();
 			BinanceSymbol symbol = new BinanceSymbol(coin.key);
 			BinanceOrderPlacement placement = new BinanceOrderPlacement(symbol, BinanceOrderSide.BUY);
-			placement.setType(BinanceOrderType.MARKET);
-			placement.setPrice(BigDecimal.valueOf(coin.buyPrice));
-			double quantity = totalBalance * assetRate / coin.buyPrice;
+			placement.setType(BinanceOrderType.LIMIT);
+			double buyPrice = Double.valueOf(coin.priceStep.format(coin.buyPrice * 1.02));
+			double quantity = Double.valueOf(coin.quantityStep.format(totalBalance * assetRate / buyPrice));
+
+			if (buyPrice < coin.minPrice || quantity < coin.minQty) {
+				sendMsgToTelegram("Order Fail. Price : " + buyPrice + ", Quantity : " + quantity, true);
+				return;
+			}
+			placement.setPrice(BigDecimal.valueOf(buyPrice));
 			placement.setQuantity(BigDecimal.valueOf(quantity));
 			BinanceOrder order = api.getOrderById(symbol, api.createOrder(placement).get("orderId").getAsLong());
-			sendMsgToTelegram(order.toString());
-			coin.isReallyBuy = true;
+			sendMsgToTelegram(order.toString(), true);
+			if (!order.toString().contains("ERROR")) {
+				String price = order.toString();
+				price = price.substring(price.indexOf("price="));
+				price = price.substring(price.indexOf("=")+1, price.indexOf(","));
+				coin.buyPrice = Double.valueOf(price);
+				boughtCnt++;
+			} else {
+				coin.buyPrice = 0;
+			}
 		} catch (BinanceApiException e) {
-			sendMsgToTelegram(e.getMessage());
+			sendMsgToTelegram(e.getMessage(), true);
 		}
 	}
 
@@ -234,16 +249,95 @@ public class main {
 			BinanceApi api = new BinanceApi();
 			BinanceSymbol symbol = new BinanceSymbol(coin.key);
 			BinanceOrderPlacement placement = new BinanceOrderPlacement(symbol, BinanceOrderSide.SELL);
-			placement.setType(BinanceOrderType.MARKET);
-			placement.setPrice(BigDecimal.valueOf(coin.cutPrice));
-			double quantity = new BinanceApi().balancesMap().get(coin.key.substring(0, coin.key.indexOf("ETH"))).free
-					.doubleValue();
+			placement.setType(BinanceOrderType.LIMIT);
+			double sellPrice = Double.valueOf(coin.priceStep.format(coin.cutPrice * 0.98));
+			double quantity = Double.valueOf(coin.quantityStep
+					.format(new BinanceApi().balancesMap().get(coin.key.substring(0, coin.key.indexOf("ETH"))).free
+							.doubleValue()));
+
+			if (sellPrice < coin.minPrice || quantity < coin.minQty) {
+				sendMsgToTelegram("Order Fail. Price : " + sellPrice + ", Quantity : " + quantity, true);
+				return;
+			}
+
+			placement.setPrice(BigDecimal.valueOf(sellPrice));
 			placement.setQuantity(BigDecimal.valueOf(quantity));
 			BinanceOrder order = api.getOrderById(symbol, api.createOrder(placement).get("orderId").getAsLong());
-			sendMsgToTelegram(order.toString());
-			coin.isReallyBuy = false;
+			sendMsgToTelegram(order.toString(), true);
+			if (!order.toString().contains("ERROR")) {
+				boughtCnt--;
+				coin.buyPrice = 0;
+			}
 		} catch (BinanceApiException e) {
-			sendMsgToTelegram(e.getMessage());
+			sendMsgToTelegram(e.getMessage(), true);
+		}
+	}
+
+	public static void initCoinInfo() {
+		try {
+			JsonObject obj = new BinanceRequest("https://www.binance.com/api/v1/exchangeInfo").read().asJsonObject();
+			JsonArray array = obj.getAsJsonArray("symbols");
+			for (int i = 0; i < array.size(); i++) {
+				JsonObject info = array.get(i).getAsJsonObject();
+				String coinName = info.get("symbol").toString().replaceAll("\"", "");
+				if (coinName.equals("123456") || !coinName.endsWith("ETH")) {
+					continue;
+				}
+				JsonArray filters = info.getAsJsonArray("filters");
+				String minQty = "";
+				String stepSize = "";
+				String minPrice = "";
+				String stepPrice = "";
+				for (int k = 0; k < filters.size(); k++) {
+					if (filters.get(k).getAsJsonObject().get("filterType").toString().replaceAll("\"", "")
+							.equals("LOT_SIZE")) {
+						minQty = filters.get(k).getAsJsonObject().get("minQty").toString().replaceAll("\"", "");
+						stepSize = filters.get(k).getAsJsonObject().get("stepSize").toString().replaceAll("\"", "");
+					}
+					if (filters.get(k).getAsJsonObject().get("filterType").toString().replaceAll("\"", "")
+							.equals("PRICE_FILTER")) {
+						minPrice = filters.get(k).getAsJsonObject().get("minPrice").toString().replaceAll("\"", "");
+						stepPrice = filters.get(k).getAsJsonObject().get("tickSize").toString().replaceAll("\"", "");
+					}
+
+				}
+				String[] format = stepSize.split("\\.");
+				String formatForm = "";
+				for (int k = 0; k < format[0].length(); k++) {
+					formatForm += "#";
+				}
+				formatForm += ".";
+				for (int k = 0; k < format[1].indexOf("1") + 1; k++) {
+					formatForm += "#";
+				}
+				if (formatForm.charAt(formatForm.length() - 1) == '.') {
+					formatForm = formatForm.substring(0, formatForm.length() - 1);
+				}
+
+				String[] formatForPrice = stepPrice.split("\\.");
+				String formatFormForPrice = "";
+				for (int k = 0; k < formatForPrice[0].length(); k++) {
+					formatFormForPrice += "#";
+				}
+				formatFormForPrice += ".";
+				for (int k = 0; k < formatForPrice[1].indexOf("1") + 1; k++) {
+					formatFormForPrice += "#";
+				}
+				if (formatFormForPrice.charAt(formatFormForPrice.length() - 1) == '.') {
+					formatFormForPrice = formatFormForPrice.substring(0, formatFormForPrice.length() - 1);
+				}
+
+				NumberFormat formatter = new DecimalFormat(formatForm);
+				formatter.setRoundingMode(RoundingMode.DOWN);
+
+				NumberFormat formatterForPrice = new DecimalFormat(formatFormForPrice);
+				formatterForPrice.setRoundingMode(RoundingMode.DOWN);
+
+				coins.put(coinName, new CoinInfo(coinName, Double.valueOf(minQty), formatter, Double.valueOf(minPrice),
+						formatterForPrice));
+			}
+		} catch (BinanceApiException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -251,10 +345,17 @@ public class main {
 		public String key;
 		public double buyPrice;
 		public double cutPrice;
-		public boolean isReallyBuy = false;
+		public double minQty;
+		public NumberFormat quantityStep;
+		public double minPrice;
+		public NumberFormat priceStep;
 
-		public CoinInfo(String key) {
+		public CoinInfo(String key, double minQty, NumberFormat step, double minPrice, NumberFormat priceStep) {
 			this.key = key;
+			this.minQty = minQty;
+			this.quantityStep = step;
+			this.minPrice = minPrice;
+			this.priceStep = priceStep;
 		}
 	}
 
