667147e6380adc05742f0321a7bd1dbd7a7d8bad
chongjae <yoocj88@naver.com>
2018-02-06 08:32:35 +0900

    fix db using

--- src/main/java/com/chongjae/Main.java
+++ src/main/java/com/chongjae/Main.java
@@ -608,8 +608,6 @@ public class Main {
 	public static void saveBoughtInfo(CoinInfo coin) {
 		Connection con = null;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql:localhost:3306/coinInfo" , "coin", "coin1234");
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/coinInfo?serverTimezone=UTC" , "coin", "coin1234");
 		} catch (Exception e) {
